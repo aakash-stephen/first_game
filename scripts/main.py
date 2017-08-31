43e10c8212a86e3cdbdf65770d5b3f856e422cc1
aakash-stephen <aakashjeremiah@hotmail.com>
2017-09-01 02:25:44 +0530

    jump fixed

--- scripts/main.py
+++ scripts/main.py
@@ -13,19 +13,6 @@ background = pygame.image.load( img_path + 'background1.png')
 kaboom = pygame.image.load( img_path + 'kaboom.png')
 fireball = pygame.image.load ( img_path + 'fireball.png' )
 clock = pygame.time.Clock()
-game_quit = False
-def player_motion(x1,y1,f=0):
-    if f == 1:
-        screen.blit( player , (x1,y1))
-        for i in range(-20,21):
-            pygame.time.delay(10)
-            y1 += i/2 
-            player_motion(x1,y1)
-            screen.blit( player , (x1,y1))
-            pygame.display.update()
-    screen.blit( player , (x1,y1))
-def bowser_motion(x2,y2):
-    screen.blit( bowser , (x2,y2))
 x1 = width * 0.20
 y1 = height * 0.85 -80
 x2 = width * 0.75
@@ -34,15 +21,35 @@ x1_change = 0
 y1_change = 0
 x2_change = 0
 y2_change = 0
+f = 0
+game_quit = False
+cur_time = 0
+def player_motion(x1,y1,f=0,elap=1001):
+    if f == 1 and elap < 1.5:
+        screen.blit( player , (x1,y1))
+        y_change = 5
+    else:
+        screen.blit( player , (x1,y1)) 
+        """for i in range(-20,21):
+            pygame.time.delay(10)
+            y1 += i/2 
+            player_motion(x1,y1)
+            screen.blit( player , (x1,y1))
+            pygame.display.update()"""
+        pygame.display.update()
+def bowser_motion(x2,y2):
+    screen.blit( bowser , (x2,y2))
+
 while not game_quit:
     screen.fill( (255, 255, 255) ) 
     floor = pygame.draw.rect( screen , (128,0,0) , (0 , height * 0.85 , width , 0.2 * height))  
     for event in pygame.event.get():
-        f = 0
-        print(event)
+       
+        #print(event)
         if event.type == pygame.QUIT:
             game_quit = True
         if event.type == pygame.KEYDOWN:
+            f = 0
             if event.key == pygame.K_a: 
                 x1_change = -5
             if event.key == pygame.K_d: 
@@ -53,12 +60,8 @@ while not game_quit:
                 #y1_change = -5
             if event.key == pygame.K_SPACE:
                 f = 1
-                """for i in range(-20,21):
-                    pygame.time.delay(10)
-                    y1 += i/2 
-                    player_motion(x1,y1)
-                    bowser_motion(x2,y2)
-                    pygame.display.update()"""
+                cur_time = time.clock()
+                y1_change = -5
             if event.key == pygame.K_LEFT: 
                 x2_change = -5
             if event.key == pygame.K_RIGHT: 
@@ -70,28 +73,31 @@ while not game_quit:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_s or event.key == pygame.K_w:
                 x1_change = 0
-                #y1_change = 0
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+            if event.key == pygame.K_SPACE:
+                y1_change = 5
+            """if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                 x2_change = 0
-                #y2_change = 0
+                #y2_change = 0"""
         
-    
+    #print("1")
     if (x1 > 0 and x1 + 80 < width) or (x1 == 0 and x1_change > 0) or ( x1 + 80 == width and x1_change <0):
         x1 += x1_change
-    #if (y1 > 0 and y1 + 80 < 0.85*height) or (y1 == 0 and y1_change > 0) or ( y1 + 80 == 0.85*height and y1_change <0):
-        #y1 += y1_change
-    if (x2 > 0 and x2 + 90 < width) or (x2 == 0 and x2_change > 0) or ( x2 + 90 == width and x2_change <0):
+    if (y1 > 0 and y1 + 80 < 0.85*height) or (y1 == 0 and y1_change > 0) or ( y1 + 80 == 0.85*height and y1_change <0):
+        y1 += y1_change
+    """if (x2 > 0 and x2 + 90 < width) or (x2 == 0 and x2_change > 0) or ( x2 + 90 == width and x2_change <0):
         x2 += x2_change
-    #if (y2 > 0 and y2 + 90 < 0.85*height) or (y2 == 0 and y2_change > 0) or ( y2 + 90 == 0.85*height and y2_change <0):
-        #y2 += y2_change
-    """if f == 1:
+    if (y2 > 0 and y2 + 90 < 0.85*height) or (y2 == 0 and y2_change > 0) or ( y2 + 90 == 0.85*height and y2_change <0):
+        y2 += y2_change
+    if f == 1:
         for i in range(-20,21):
              pygame.time.delay(10)
              y1 += i/2 
              player_motion(x1,y1)
              #bowser_motion(x2,y2)
              pygame.display.update() """         
-    player_motion(x1,y1,f)           
+    elap = time.clock() - cur_time
+    #print(elap)
+    player_motion(x1,y1,f,elap)           
     #bowser_motion(x2,y2)
     pygame.display.update()
     clock.tick(60)

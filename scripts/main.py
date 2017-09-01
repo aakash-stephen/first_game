9572d05f5196a9874f4323985c47761f52a0ebfe
aakash-stephen <aakashjeremiah@hotmail.com>
2017-09-01 14:30:49 +0530

    plaforms 1,2,3

--- scripts/main.py
+++ scripts/main.py
@@ -25,10 +25,14 @@ y2_change = 0
 f = 0
 game_quit = False
 cur_time = 0
+platform_height = 0.02* height
+platform_xpos1 = width * 0.1
+platform_xpos2 = width * 0.3
+platform_ypos = height * 0.21
 def player_motion(x1,y1,f=0,elap=1001):
     if f == 1 and elap < 1.5:
         screen.blit( player , (x1,y1))
-        y_change = 10
+        #y_change = 10
     else:
         screen.blit( player , (x1,y1)) 
         """for i in range(-20,21):
@@ -43,7 +47,10 @@ def bowser_motion(x2,y2):
 
 while not game_quit:
     screen.fill( (255, 255, 255) ) 
-    floor = pygame.draw.rect( screen , (128,0,0) , (0 , height * 0.85 , width , 0.2 * height))  
+    floor = pygame.draw.rect( screen , (128,0,0) , (0 , height * 0.85 , width , 0.15 * height))  
+    platform1 = pygame.draw.rect( screen , (128,0,0) , (platform_xpos1 , platform_ypos * 1 , width*0.1 , platform_height))
+    platform2 = pygame.draw.rect( screen , (128,0,0) , (platform_xpos2 , platform_ypos * 2 , width*0.1 , platform_height))
+    platform3 = pygame.draw.rect( screen , (128,0,0) , (platform_xpos1 , platform_ypos * 3 , width*0.1 , platform_height))
     for event in pygame.event.get():
        
         #print(event)
@@ -83,7 +90,7 @@ while not game_quit:
     #print("1")
     if (x1 > 0 and x1 + 80 < width) or (x1 == 0 and x1_change > 0) or ( x1 + 80 == width and x1_change <0):
         x1 += x1_change
-    if (y1 > 0 and y1 + 80 < 0.85*height) or (y1 == 0 and y1_change > 0) or ( y1 + 80 == 0.85*height and y1_change <0):
+    if ((y1 > 0 and y1 + 80 < 0.85 * height) or (y1 == 0 and y1_change > 0) or ( y1 + 80 == 0.85*height and y1_change <0)):
         y1 += y1_change
     """if (x2 > 0 and x2 + 90 < width) or (x2 == 0 and x2_change > 0) or ( x2 + 90 == width and x2_change <0):
         x2 += x2_change

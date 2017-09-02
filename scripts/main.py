4b6385b322cd167936325c07eec818f310d98d52
aakash-stephen <aakashjeremiah@hotmail.com>
2017-09-02 17:19:00 +0530

    platforms and fireball trial

--- scripts/main.py
+++ scripts/main.py
@@ -15,7 +15,7 @@ kaboom = pygame.image.load( img_path + 'kaboom.png')
 fireball = pygame.image.load ( img_path + 'fireball.png' )
 clock = pygame.time.Clock()
 x1 = width * 0.20
-y1 = height * 0.85 -80
+
 x2 = width * 0.75
 y2 = height * 0.85 -90
 x1_change = 0
@@ -31,8 +31,8 @@ platform_xpos2 = width * 0.3
 platform_xpos4 =width -width*0.4
 platform_xpos6=width-width*0.2
 platform_ypos = height * 0.21
-
-
+y1 = height * 0.85 - 80
+g = 5
 def player_motion(x1,y1,f=0,elap=1001):
     if f == 1 and elap < 1.5:
         screen.blit( player , (x1,y1))
@@ -51,6 +51,7 @@ def bowser_motion(x2,y2):
 
 while not game_quit:
     #screen.fill( (255, 255, 255) )
+    g = 5
     screen.blit( background , (0,0))
     floor = pygame.draw.rect( screen , (128,0,0) , (0 , height * 0.85 , width , 0.15 * height))
     platform1 = pygame.draw.rect( screen , (128,0,0) , (platform_xpos1 , platform_ypos * 1 , width*0.1 , platform_height))
@@ -64,6 +65,7 @@ while not game_quit:
         #print(event)
         if event.type == pygame.QUIT:
             game_quit = True
+        
         if event.type == pygame.KEYDOWN:
             f = 0
             if event.key == pygame.K_a:
@@ -74,7 +76,7 @@ while not game_quit:
                 #y1_change = 5
             #if event.key == pygame.K_w:
                 #y1_change = -5
-            if event.key == pygame.K_SPACE and y1 == height * 0.85 -80:
+            if event.key == pygame.K_SPACE: #and y1 == height * 0.85 -80:
                 f = 1
                 cur_time = time.clock()
                 y1_change = -10
@@ -90,18 +92,27 @@ while not game_quit:
             if event.key == pygame.K_a or event.key == pygame.K_d or event.key == pygame.K_s or event.key == pygame.K_w:
                 x1_change = 0
             if event.key == pygame.K_SPACE:
-                y1_change = 5
+                y1_change = 0
+            
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                 x2_change = 0
                 #y2_change = 0
-
     #print("1")
     if (x1 > 0 and x1 + 80 < width) or (x1 == 0 and x1_change > 0) or ( x1 + 80 == width and x1_change <0):
         x1 += x1_change
-    if ((y1 > 0 and y1 + 80 < 0.85 * height) or (y1 == 0 and y1_change > 0) or ( y1 + 80 == 0.85*height and y1_change <0)):
-        y1 += y1_change
+    if (y1 + 80 > 0.85 * height):
+        #if (y1 + 80 == platform_ypos * 3 and platform_xpos1 <= x1 + 40 and x1 <= width*0.2 +40 and y1_change < 0):
+        g = 0
+        #else:
+            #if y1 + 80 == platform_ypos * 3 and platform_xpos1 <= x1 + 40 and x1 <= width*0.2 +40 ==0:
+                #y1 += y1_change
+    if (((y1 + 80 < platform_ypos * 3 + 10 and y1 + 80 > platform_ypos * 3) or (y1 + 80 < platform_ypos + 10 and y1 + 80 > platform_ypos))  and platform_xpos1 <= x1 + 30 and x1 + 10 <= width*0.2 ):
+        g = 0
+    if ((y1 + 80 < platform_ypos * 2 + 10 and y1 + 80 > platform_ypos * 2) and platform_xpos2 <= x1 +30 and x1 + 10 <= width*0.4):
+        g = 0
     if (x2 > 0 and x2 + 90 < width) or (x2 == 0 and x2_change > 0) or ( x2 + 90 == width and x2_change <0):
         x2 += x2_change
+    y1 += y1_change + g
     """if (y2 > 0 and y2 + 90 < 0.85*height) or (y2 == 0 and y2_change > 0) or ( y2 + 90 == 0.85*height and y2_change <0):
         y2 += y2_change
     if f == 1:
--- scripts/trial.py
+++ scripts/trial.py
@@ -1,2 +1,10 @@
-import os
-dir = os.path.dirname()
+import pygame
+from pygame.locals import *
+
+pygame.init
+screen = pygame.display.set_mode( (1200,600))
+fireball= pygame.image.load("../resources/images/fireball1.png")
+while True:
+    screen.blit( fireball , (0,0))
+    pygame.display.update()
+
